6c0092054ccdb8c1c3e79ecc7349b291fffa29c4
HugoTorres-ASIM <hugo.torres@student.agoraism.com>
2023-01-30 12:49:55 +0100

    ads

--- c2.py
+++ c2.py
@@ -1,13 +1,16 @@
 #Requeriments: printc.py
 from printc import printc, binput
 printc('[37m[44m ! [0m Inserte el primer número.[0m')
-arr = binput()
+arrn1 = binput()
+arr = []
+arr.append(arrn1)
 ###########################################
 #             Array + Printc              #
 ###########################################
-
-
+def addn(num):
+    global arr
+    arr.append(num)
 ###########################################
 while True:
-    printc('[37m[44m ! [0m Inserte el primer número.[0m')
+    printc('[37m[44m ! [0m Inserte otro número. (Use: cancel para finalizar.)[0m')
     numb = binput()
