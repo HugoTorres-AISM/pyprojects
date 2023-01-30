82664c301f615c2ad8aad5f520f51235c8d432ed
HugoTorres-ASIM <hugo.torres@student.agoraism.com>
2023-01-30 12:40:45 +0100

    added uwu

--- c1.py
+++ c1.py
@@ -1,7 +1,10 @@
-#IMPORTANT: DOWNLOAD THE hyglib FILE!
-from printc import printc, binput
+#Requeriments: printc.py
+from printc import printc, binput, getn
 printc('[37m[44m ! [0m Inserte el primer número.[0m')
-
+n1 = binput()
 printc('[37m[44m ! [0m Inserte el segundo número.[0m')
-
+n2 = binput()
 printc('[37m[44m ! [0m Inserte el tercer número.[0m')
+n3 = binput()
+ans = getn(n1, n2, n3)
+printc('[37m[44m ! [0m El número más grande es: [0m'+ans)
--- printc.py
+++ printc.py
@@ -1,6 +1,17 @@
-def printc(str):
-    import os
-    os.system('echo '+str)
-def printc():
-    a = input('> ')
-    return a
+def printc (str ):#line:1
+    import os #line:2
+    os .system ('echo '+str )#line:3
+def binput ():#line:4
+    O000O0OOOOOOO000O =input ('> ')#line:5
+    return O000O0OOOOOOO000O #line:6
+def getn (n1 ,n2 ,n3 ):#line:7
+    if (n1 >n2 and n1 >n3 ):#line:8
+        return n1 #line:9
+    else :#line:10
+        if (n2 >n3 ):#line:11
+            return n2 #line:12
+        else :#line:13
+            if (n1 <n3 ):#line:14
+                return n3 #line:15
+            else :#line:16
+                return n1 
