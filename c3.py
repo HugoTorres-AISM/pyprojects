8a7cbbed0790f8dd4070a38ebe4b44ed34f3e48a
HugoTorres-ASIM <hugo.torres@student.agoraism.com>
2023-01-31 09:42:03 +0100

    Added c3.py

---
+++ c3.py
@@ -0,0 +1,11 @@
+from printc import printc, binput
+printc('[37m[44m ! [0m Inserte algo interesante. (1/2)[0m')
+str1 = binput()
+printc('[37m[44m ! [0m Inserte algo interesante. (2/2)[0m')
+str2 = binput()
+lstr1 = len(str1)
+lstr2 = len(str2)
+if (lstr1 > lstr2):
+    printc('[37m[44m ! [0m Me aburres: [0m'+str1)
+else:
+    printc('[37m[44m ! [0m Me aburres: [0m'+str2)
