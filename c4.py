fc9d9c5f214d0709dd2bcc3098408bf547cee4f5
HugoTorres-ASIM <hugo.torres@student.agoraism.com>
2023-02-06 12:41:49 +0100

    :)

--- c4.py
+++ c4.py
@@ -1,8 +1,4 @@
 from printc import *
-###########################################################
-def intersection(list_a, list_b):
-    return [ e for e in list_a if e in list_b ]
-###########################################################
 printb('Inserte lo que quiera, me da igual, pero separe cada cosa con comas y un espacio, ya que si no me das cringe. (, ) - Limite de 128 elementos por fastidiar. (0/1)')
 string1 = binput()
 l1 = string1.split(', ', 128)
--- printc.py
+++ printc.py
@@ -24,3 +24,5 @@ def getn(n1 ,n2 ,n3 ):#line:7
     except ValueError as e:
         printc('[37m[41m ! [0m Hubo un error inesperado y el número no puedo ser obtenido. | [0m',e)
         return -1
+def intersection(list_a, list_b):
+    return [ e for e in list_a if e in list_b ]
