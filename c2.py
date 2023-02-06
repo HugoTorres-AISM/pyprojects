58d550ca6e76ab62110f32b90b5b14582a495249
HugoTorres-ASIM <hugo.torres@student.agoraism.com>
2023-02-06 12:28:32 +0100

    Fixed and finished C2

--- c2.py
+++ c2.py
@@ -10,6 +10,12 @@ arr.append(arrn1)
 def addn(num):
     global arr
     arr.append(num)
+def sum(arr):
+    sum = 0
+    for i in arr:
+        iint = int(i)
+        sum = sum + iint
+    return(sum)
 ###########################################
 while True:
     printc('[37m[44m ! [0m Inserte otro número. (Use: cancel para finalizar.)[0m')
@@ -17,5 +23,15 @@ while True:
     if (numb == 'cancel'):
         break
     else:
-        addn(numb)
-printc(string(arr))
+        integ = 'true'
+        try:
+            xcf = int(numb)
+        except:
+            printc('[37m[44m ! [0m No mijo, esto no. [0m')
+            integ = 'false'
+        if (integ != 'false'):  
+            addn(numb)
+        
+arrn = ",".join(str(x) for x in arr)
+printc('[37m[44m ! [0m Array: [0m' + arrn)
+printc('[37m[44m ! [0m Resultado (Aprende a sumar): [0m' + str(sum(arr)))
