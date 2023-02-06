6ab555a99460da079074c4924d5facafe77d1ad5
HugoTorres-ASIM <hugo.torres@student.agoraism.com>
2023-02-06 12:13:02 +0100

    Added ==

--- c3.py
+++ c3.py
@@ -8,4 +8,7 @@ lstr2 = len(str2)
 if (lstr1 > lstr2):
     printc('[37m[44m ! [0m Me aburres: [0m'+str1)
 else:
-    printc('[37m[44m ! [0m Me aburres: [0m'+str2)
+    if (lstr1 == lstr2):
+        printc('[37m[44m ! [0m Me aburres, son las dos igualitas.[0m')
+    else:
+        printc('[37m[44m ! [0m Me aburres: [0m'+str2)
