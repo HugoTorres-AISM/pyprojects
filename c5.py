b746f4e748725d0d579ed863a4427a3c421195e5
HugoTorres-ASIM <hugo.torres@student.agoraism.com>
2023-02-14 09:47:24 +0100

    Added c5.

--- c5.py
+++ c5.py
@@ -1,9 +1,31 @@
 from printc import *
-v = 0
-printb('Inserte todo lo que quiera.')
-string = binput()
-for x in string:
-    if (x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u' or x == 'A' or x == 'E' or x == 'I' or x == 'O' or x == 'U'):
-        v = v+1
-        printb('Vocal detectada ('+str(x)+')')
-printb('Que si, Keroro, que si, que es/son '+str(v)+' vocal(es) y punto.')
+clear()
+printb('Inserte un numerito')
+x1 = binput()
+while True:
+    if (str.isdigit(x1)):
+        if ('-' in x1):
+            clear()
+            printb('No me toques los integrales, vuelve otra vez.')
+            x1 = binput()  
+        else:
+            if (x1 == 0):
+                clear()
+                printb('No me toques los integrales, vuelve otra vez.')
+                x1 = binput()  
+            else:
+                break
+    else:
+        clear()
+        printb('No me toques los integrales, vuelve otra vez.')
+        x1 = binput()
+clear()
+rest1 = 1
+rest2 = 1
+res = 1
+while (int(rest2) <= int(x1)):
+    res = rest1*res
+    rest1 = rest1+1
+    rest2 = rest2+1
+    printb('Estado: '+str(rest1-1)+'/'+str(x1))
+printb(str(res))
